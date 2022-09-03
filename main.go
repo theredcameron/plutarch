acde736a355743bddfce6f0b848ea93f81e4df68
Mark <435089@gmail.com>
2022-09-03 00:22:53 -0500

    added finishing touches

--- main.go
+++ main.go
@@ -3,7 +3,14 @@ package main
 import (
 	"fmt"
 	"os"
+	"io"
 	"strings"
+	"sync"
+	"net/http"
+	"time"
+	"github.com/go-xmlfmt/xmlfmt"
+	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/plumbing/object"
 )
 
 func check(e error) {
@@ -14,21 +21,75 @@ func check(e error) {
 
 func main() {
 	//Read file for list of web pages to save
-	data, err := os.ReadFile("sites")
+	data, err := os.ReadFile("sites.list")
 	check(err)
 
 	content := string(data)
 	lines := strings.Split(content, "\n")
 
-	for index := 0; index < len(lines); index++ {
-		fmt.Println(lines[index])
-		
+	r, err := git.PlainOpen("plutarchs-journal")
+	check(err)
+
+	w, err := r.Worktree()
+	check(err)
+
+	var wg sync.WaitGroup
+
+	getAndSave := func(index int, lines []string) {
+		currentTime := time.Now().Format("2006-01-02T15:04:05.000000")
+
+		line := strings.Split(lines[index], ":::")
+
+		if len(line) != 2 {
+			panic("URL file formatted incorrectly")
+		}
+
+		fmt.Printf("[%v] - %v - %v\n", currentTime, line[0], line[1])
+
+		response, err := http.Get(line[1])
+		check(err)
+		defer response.Body.Close()
+
+		body, err := io.ReadAll(response.Body)
+		check(err)
+
+		bodyString := string(body)
+
+		prettyBody := xmlfmt.FormatXML(bodyString, "\t", " ")
+
+		err = os.WriteFile(fmt.Sprint("plutarchs-journal/", line[0]), []byte(prettyBody), 0666)
+		check(err)		
+
+		_, err = w.Add(line[0])
+		check(err)
+
+		commit, err := w.Commit("Changes to file", &git.CommitOptions{
+			Author: &object.Signature{
+				Name: "Mark Ehresman",
+				Email: "435089@gmail.com",
+				When: time.Now(),
+			},
+		})
+		check(err)
+
+		_, err = r.CommitObject(commit)
+		check(err)
+
+		wg.Done()
 	}
-		//ASYNC - For each web page, get contents and save.
-			//Get page content
-			//If page file exists, overrite.
-			//Else, create new file
-			//Git add and commit
-			//Push value through channel to signify task finished
-	//When all tasks are finished, Git push repo
+
+	wg.Add(len(lines))
+
+	for index := 0; index < len(lines); index++ {
+		go getAndSave(index, lines)
+	}
+
+	fmt.Println("Waiting for jobs to finish...")
+	wg.Wait()
+	fmt.Println("Jobs finished")
+
+	fmt.Println("Pushing site changes...")
+	err = r.Push(&git.PushOptions{})
+	check(err)
+	fmt.Println("Site changes pushed")
 }
